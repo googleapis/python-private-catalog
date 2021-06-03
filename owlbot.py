8c6f16b9c37d0cd2dd9cdf37217e9dd93cfd9d15
Anthonios Partheniou <partheniou@google.com>
2021-06-03 00:24:37 +0000

    chore: set coverage level to 97

--- owlbot.py
+++ owlbot.py
@@ -32,7 +32,7 @@ s.remove_staging_dirs()
 # Add templated files
 # ----------------------------------------------------------------------------
 
-templated_files = common.py_library(cov_level=99, microgenerator=True)
+templated_files = common.py_library(cov_level=97, microgenerator=True)
 
 s.move(templated_files, excludes=["setup.py", "README.rst", "docs/index.rst"])
 
